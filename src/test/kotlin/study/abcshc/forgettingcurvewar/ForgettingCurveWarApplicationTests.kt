e954e97f7ce1170f63c04d514249fcd49c7ae2fe
Heechul So <thrrkthrrk@gmail.com>
2022-07-23 16:54:02 +0900

    🧹 initialize

---
+++ src/test/kotlin/study/abcshc/forgettingcurvewar/ForgettingCurveWarApplicationTests.kt
@@ -0,0 +1,13 @@
+package study.abcshc.forgettingcurvewar
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ForgettingCurveWarApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
